ae729083fe002869cc9d4d98005a8abcc4d27f5b
it21109126 <it21109126@my.sliit.lk>
2022-10-23 23:10:31 +0530

    Today's commit

--- app/src/main/java/com/example/xpensemobileapp/MainActivity.java
+++ app/src/main/java/com/example/xpensemobileapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.xpensemobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startNext(View view){
+        Intent intent = new Intent(MainActivity.this, ExpenseFormActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void onClickBtn3(View view){
+        Intent intent = new Intent(MainActivity.this, ExpenseOverviewActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void onClickLastBtn(View view){
+        Intent intent = new Intent(MainActivity.this, ExpensesDashboardActivity.class);
+
+       startActivity(intent);
     }
 }
